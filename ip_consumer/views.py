b3dcf73123f2eb385b3e15a11e0f2077ac433758
Omar Bendary <omar.bendary.jr@gmail.com>
2024-02-16 14:09:20 +0200

    remove unused imports

--- ip_consumer/views.py
+++ ip_consumer/views.py
@@ -1,10 +1,7 @@
-# views.py
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import IPAddress
 from .serializers import IPAddressSerializer
 
 
