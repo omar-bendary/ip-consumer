6dd0039b625c77006558db40c15221b8d95ed073
Omar Bendary <omar.bendary.jr@gmail.com>
2024-02-16 17:00:32 +0200

    remove unused code comments

--- ip_consumer/consumers.py
+++ ip_consumer/consumers.py
@@ -2,37 +2,6 @@ import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# from ip_consumer.serializers import IPAddressSerializer
-
-# from ip_consumer.tasks import get_ip_details
-
-# class IPAddressConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         await self.accept()
-
-#     async def disconnect(self, close_code):
-#         pass
-
-#     async def receive(self, text_data):
-#         from time import sleep
-
-#         text_data_json = json.loads(text_data)
-#         ip_addresses = text_data_json.get("ip_addresses", [])
-#         serializer = IPAddressSerializer(data=text_data_json)
-#         serializer.is_valid(raise_exception=True)
-
-#         for ip in ip_addresses:
-#             try:
-#                 ip_detail = get_ip_details(ip)
-
-#                 await self.send(text_data=json.dumps({ip: ip_detail}))
-#                 sleep(1)
-#             except Exception as e:
-#                 await self.send(text_data=json.dumps({"error": str(e)}))
-
-
-# consumers.py
-
 
 class IPAddressConsumer(AsyncWebsocketConsumer):
     async def connect(self):
